b6098ab88949daf8e72be89fbd1fcb8e73c14e74
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-08 14:24:46 +0200

    feat: header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <h1 class="text-center">Home Page</h1>
     <section>
-
+        
     </section>
 @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="img-fluid">
+        <img src="{{Vite::asset('resources/img/boolean-logo.png')}}" alt="Logo Boolando">
+    </div>
+</header>
