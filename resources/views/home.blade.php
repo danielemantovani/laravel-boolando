4d3aab708b95ff80cad843453183a1e993dfc727
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-09 12:29:09 +0200

    feat: heart in card

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,12 @@
         <div class="row g-3 mt-5 mb-5">
             @foreach ($products as $product)
                 <div class="col-4">
-                    <div class="card">
-                        <img src="{{ Vite::asset("resources/img/".$product['frontImage']) }}" alt="">
+                    <div class="card position-relative">
+                        <img src="{{ Vite::asset("resources/img/".$product['frontImage']) }}" alt="{{$product['name']}}">
+                        <span class="position-absolute fs-3 px-2 ms_heart ">&hearts;</span>
+                        @foreach ($product['badges'] as $badge)
+                            
+                        @endforeach
                         <div class="card-body">
                             <p>{{$product['brand']}}</p>
                             <h4>{{$product['name']}}</h4>
