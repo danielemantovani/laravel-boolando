dfe9ad3e47d346087fbf382f7227107bdf22712c
danielemantovani <daniele.mantovani124@gmail.com>
2024-06-09 12:51:48 +0200

    fix: favourites products

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,11 +7,15 @@
                 <div class="col-4">
                     <div class="card position-relative">
                         <img src="{{ Vite::asset("resources/img/".$product['frontImage']) }}" alt="{{$product['name']}}">
-                        <span class="position-absolute fs-3 px-2 ms_heart ">&hearts;</span>
+                        @if ($product ['isInFavorites'] === true)
+                            <span class="position-absolute fs-3 px-2 ms_heart ms_heart-fovourite ">&hearts;</span>
+                        @else
+                            <span class="position-absolute fs-3 px-2 ms_heart">&hearts;</span>
+                        @endif
                         @foreach ($product['badges'] as $badge)
-                            
+                            <p class="position-absolute ms_badge">{{$badge['value']}}</p>
                         @endforeach
-                        <div class="card-body">
+                        <div class="card-body ms_card-body">
                             <p>{{$product['brand']}}</p>
                             <h4>{{$product['name']}}</h4>
                             <p class="text-danger fw-bold">{{$product['price']}}</p>
